b693da2140ec10ebfde5cb45dca2c684cd103456
Bhupendra <bhupendrapatel1992@gmail.com>
2020-08-19 11:16:58 +0530

    constant file update

--- frontend/src/app/common/constants.ts
+++ frontend/src/app/common/constants.ts
@@ -3,3 +3,5 @@ export const BaseUrl = "http://localhost:8200/ecommerce/";
 export const signIn = "api/auth/signin";
 export const signUp = "api/auth/signup";
 export const getEmployees = "get-employees";
+export const getEmployeeById = "get-employee/";
+export const storeEmployee = "save-employee";
